ed16944632aef86ee74d9c492eb7e35890640c4d
alanarv99 <147183094+alanarv99@users.noreply.github.com>
2025-10-05 11:12:32 -0400

    reorganized js, debugged/resized  icon,

--- src/app.js
+++ src/app.js
@@ -1,64 +1,84 @@
-let apiKey = "260420cae416f4dteddo330fbd8c9c7b";
-let city = "Portland";
-let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
 function showWeather(response) {
   let dataResponse = response.data;
+
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = dataResponse.city;
-
   let countryElement = document.querySelector("#country");
-  countryElement.innerHTML = dataResponse.country;
-
   let date = new Date(dataResponse.time * 1000);
   let timeElement = document.querySelector("#time");
-
-  function formatDate(date) {
-    let hours = date.getHours();
-
-    let minutes = date.getMinutes();
-    if (minutes < 10) {
-      minutes = `0$minutes`;
-    }
-
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[date.getDay()];
-
-    return `${day}, ${hours}:${minutes}`;
-  }
-
-  timeElement.innerHTML = formatDate(date);
-
   let temperature = dataResponse.temperature;
   let cToF = (temperature.current * 9) / 5 + 32;
   let feelsCToF = (temperature.feels_like * 9) / 5 + 32;
-
-  let iconElement = document.querySelector("#todayIcon");
-  iconElement.innerHTML = `<img src="${dataResponse.condition.icon_url}" class=""todayWeatherIcon alt="${dataResponse.condition.icon}">`;
-
+  let iconElement = document.querySelector("#todayWeatherIcon");
+  let iconUrl = dataResponse.condition.icon_url;
   let temperatureFElement = document.querySelector("#todayTempF");
-  temperatureFElement.innerHTML = Math.round(cToF);
   let temperatureCElement = document.querySelector("#todayTempC");
-  temperatureCElement.innerHTML = Math.round(temperature.current);
-
   let todayFeelsLikeFElement = document.querySelector("#todayFeelsLikeF");
-  todayFeelsLikeFElement.innerHTML = Math.round(feelsCToF);
   let todayFeelsLikeCElement = document.querySelector("#todayFeelsLikeC");
-  todayFeelsLikeCElement.innerHTML = Math.round(temperature.feels_like);
-
   let humidityElement = document.querySelector("#todayHumidity");
-  humidityElement.innerHTML = temperature.humidity;
-
   let windElement = document.querySelector("#todayWind");
+
+  cityElement.innerHTML = dataResponse.city;
+  countryElement.innerHTML = dataResponse.country;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${iconUrl}" class="todayWeatherIcon" />`;
+  temperatureFElement.innerHTML = Math.round(cToF);
+  temperatureCElement.innerHTML = Math.round(temperature.current);
+  todayFeelsLikeFElement.innerHTML = Math.round(feelsCToF);
+  todayFeelsLikeCElement.innerHTML = Math.round(temperature.feels_like);
+  humidityElement.innerHTML = temperature.humidity;
   windElement.innerHTML = Math.round(dataResponse.wind.speed);
 }
 
-axios.get(url).then(showWeather);
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[date.getMonth()];
+  let dayOfMonth = date.getDate();
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day}, ${month} ${dayOfMonth}, ${year} at ${hours}:${minutes}`;
+}
+
+function searchCity(city) {
+  let apiKey = "260420cae416f4dteddo330fbd8c9c7b";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+function handleSearch(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#searchInput");
+
+  searchCity(searchInputElement.value);
+}
+
+let searchFormElement = document.querySelector("#searchForm");
+searchFormElement.addEventListener("submit", handleSearch);
+
+searchCity("New York");
